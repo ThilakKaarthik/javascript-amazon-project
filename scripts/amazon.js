58bae8556bcc4ee20686fd835c3e404a639008f2
Thilak Kaarthik P <thilakkaarthik@gmail.com>
2023-08-20 12:47:56 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,6 +77,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
                 quantity: 1
               });
         }
-        console.log(cart);  
+        let cartQuantity = 0;
+
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;  
     });
 });
