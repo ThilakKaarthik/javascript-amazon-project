4fc0b3273161d085abba8169e6c832ff64592c82
Thilak Kaarthik P <thilakkaarthik@gmail.com>
2023-08-29 14:36:21 +0530

    Done with some exercises

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,1 @@
-const cart = [
-    
-];
+const cart = [];
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-
+ 
 let productsHTML = '';
 products.forEach((product) => {
     productsHTML += `
@@ -25,7 +25,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
@@ -57,24 +57,31 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+const addedMessageTimeouts = {};
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
-      const productId = button.dataset.productId
+      const {productId} = button.dataset
 
         let matchingItem;
+
         cart.forEach((item) => {
             if (productId === item.productId) {
                 matchingItem = item;
             }
         });
+
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`
+        );
+        const quantity = Number(quantitySelector.value);
       
         if (matchingItem) {
-            matchingItem.quantity++;
+            matchingItem.quantity += quantity;
         }
         else {
             cart.push({
-                productId: productId,
-                quantity: 1
+                productId,
+                quantity
               });
         }
         let cartQuantity = 0;
@@ -85,5 +92,21 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         });
 
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;  
+
+        const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`
+        );
+
+        addedMessage.classList.add('added-to-cart--visible');
+
+        const previousTimeoutId = addedMessageTimeouts[productId];
+        if(previousTimeoutId) {
+            clearTimeout(previousTimeoutId);
+        }
+
+        const timeoutId = setTimeout(() => {
+            addedMessage.classList.remove('added-to-cart--visible');
+        }, 2000);
+
+        addedMessageTimeouts[productId] = timeoutId;
     });
 });
